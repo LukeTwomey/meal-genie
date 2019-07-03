2378a96cc505a0c8e2e883c40d3e33b395d4324c
Luke Twomey <luke.twomey85@gmail.com>
2019-07-03 10:32:32 +0100

    Create new API route to retrieve all recipes from database

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,13 @@ const Recipe = mongoose.model('recipe');
 
 module.exports = app => {
 
+    // Get all recipes from the database
+    app.get('/api/recipes', async (req, res) => {
+        const recipes = await Recipe.find();
+        res.send(recipes);
+    })
+
+    // Add a new recipe to the database
     app.post('/api/recipes', async (req, res) => {
         const { name, rating, cookingTime, servings, description, syns, ingredients, method } = req.body;
         const _recipe = await new Recipe({ name, rating, cookingTime, servings, description, syns, ingredients, method
