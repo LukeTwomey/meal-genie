b28d4261489051243cee042b04a62f413f8a3c3d
Luke Twomey <luke.twomey85@gmail.com>
2019-07-05 11:54:42 +0100

    Add a link to go back to all recipes

--- client/src/components/recipes/RecipeNew.js
+++ client/src/components/recipes/RecipeNew.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import RecipeForm from './RecipeForm';
+import { Link } from "react-router-dom";
 
 const RecipeNew = () => {
     return (
         <div>
             <h1>Add New Recipe</h1>
+            <Link to="/recipes">View all recipes</Link><br/><br/><br/>
             <RecipeForm />
         </div>
     )
