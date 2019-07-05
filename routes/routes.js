5c0190efb75622e37042c5ea713927595548a564
Luke Twomey <luke.twomey85@gmail.com>
2019-07-05 11:41:44 +0100

    Optimise the recipe image before saving it to the database

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const fs = require('fs');
+const optimise = require('../services/imageOptimise');
 const multer  = require('multer');
 const upload = multer({ dest: 'uploads/' })
 const Recipe = mongoose.model('recipe');
@@ -14,13 +15,22 @@ module.exports = app => {
 
     // Add a new recipe to the database. Use Multer middleware to handle multipart form data (used for image)
     app.post('/api/recipes', upload.single('image'), async (req, res) => {
-        const image = {
-            data: fs.readFileSync(req.file.path),
-            contentType: 'image/jpeg'
+        let image = null;
+
+        // Only need to do this stuff if the user has added an image file to the recipe
+        if(req.file) {
+            // Get the image file from the incoming request (with the help of Multer)
+            image = fs.readFileSync(req.file.path);
+
+            image = {
+                data: await optimise(image), // Optimise the image to reduce filesize before saving it to the db
+                contentType: 'image/jpeg'
+            }
         }
 
         const { name, rating, cookingTime, servings, description, syns, ingredients, method } = req.body;
 
+        // Save the recipe to the db
         const _recipe = await new Recipe({ name, rating, cookingTime, servings, description, syns, ingredients, method, image
         }).save();
         
