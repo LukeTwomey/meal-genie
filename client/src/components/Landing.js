3234183849bb821147540bbdb54671aabea87d1f
Luke Twomey <luke.twomey85@gmail.com>
2019-07-09 16:05:43 +0100

    Create Loading component

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 const Landing = () => {
     return (
         <div>
-            <h1>Landing page</h1>
             <Link to="/recipes">View all recipes</Link><br/>
             <Link to="/recipes/new">Add new recipe</Link>
         </div>
