edd36be8d7980301a7d348bbef38e7b8db24006e
Luke Twomey <luke.twomey85@gmail.com>
2019-07-10 12:04:47 +0100

    Integrate Font Awesome Pro

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAtom } from '@fortawesome/pro-light-svg-icons'
 
 const Landing = () => {
     return (
         <div>
+            <FontAwesomeIcon className='icon' icon={faAtom} /><br/><br/><br/>
             <Link to="/recipes">View all recipes</Link><br/>
             <Link to="/recipes/new">Add new recipe</Link>
         </div>
