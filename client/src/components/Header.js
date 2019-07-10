5e598f23df355d67b27f0d039ef34bc47623f41e
Luke Twomey <luke.twomey85@gmail.com>
2019-07-10 13:29:03 +0100

    Add icon to view all recipes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUtensils } from '@fortawesome/pro-light-svg-icons'
 import './Header.css';
 
 const Header = () => {
-  return (
-    <img src='images/logo.png' alt='logo' className='logo'/>
-  );
+    return (
+        <header>
+            <Link to="/recipes"><FontAwesomeIcon className='icon' icon={faUtensils} /></Link>
+            <Link to="/"><img src='images/logo.png' alt='logo' className='logo' /></Link>
+        </header>
+    );
 };
 
 export default Header;
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 
 const Landing = () => {
     return (
-        <div>            
-            <Link to="/recipes">View all recipes</Link><br/>
-            <Link to="/recipes/new">Add new recipe</Link>
-        </div>
+        <h1></h1>
     )
+    return null
 }
 
 export default Landing;
