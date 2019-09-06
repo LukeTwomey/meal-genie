2e2bdefd8151f70de25f0dccfe5352db26928385
Luke Twomey <luke.twomey85@gmail.com>
2019-09-06 14:47:55 +0100

    Add syns, cooking time and servings information to recipe card

--- client/src/components/recipes/RecipeCard.js
+++ client/src/components/recipes/RecipeCard.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import RecipeImage from './RecipeImage';
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngel, faClock, faUserFriends } from '@fortawesome/pro-solid-svg-icons';
 import './RecipeCard.css';
 
 const RecipeCard = (props) => {
     const { recipe } = props;
+    console.log(recipe);
     const recipeUrlName = recipe.name.replace(/\s+/g, '-').toLowerCase();
 
     return (
@@ -12,6 +15,14 @@ const RecipeCard = (props) => {
             <div className="recipeCard" >
                 <RecipeImage image={recipe.image}/>
                 <h2>{recipe.name}</h2>
+                <div className="iconContainer">
+                    <FontAwesomeIcon icon={faAngel} />
+                    <p>{recipe.syns} {recipe.syns === 1 ? "syn" : "syns"}</p>
+                    <FontAwesomeIcon icon={faClock} />
+                    <p>{`${recipe.cookingTime} mins`}</p>
+                    <FontAwesomeIcon icon={faUserFriends} />
+                    <p>{`${recipe.servings} servings`}</p>
+                </div>
             </div>
         </Link>
     )
