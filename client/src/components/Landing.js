a08d30ae1e1f48714e0e3d97c8c5ffd47e60fb81
Luke Twomey <luke.twomey85@gmail.com>
2019-09-06 10:05:15 +0100

    Remove unnecessary import

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import Loading from './Loading/Loading';
 import RecipeCard from './recipes/RecipeCard';
 
 const Landing = (props) => {
