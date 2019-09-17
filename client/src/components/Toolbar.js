843d2118ff4c530ca5612071b1a557e3520bbfbd
Luke Twomey <luke.twomey85@gmail.com>
2019-09-17 16:14:55 +0100

    Add recipe search button to recipe card toolbar

--- client/src/components/Toolbar.js
+++ client/src/components/Toolbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { toggleMealLock } from '../actions';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLockOpenAlt, faLockAlt  } from '@fortawesome/pro-solid-svg-icons';
+import { faLockOpenAlt, faLockAlt, faSearch } from '@fortawesome/pro-solid-svg-icons';
 import './Toolbar.css';
 
 const Toolbar = (props) => {
@@ -14,6 +14,9 @@ const Toolbar = (props) => {
             {locked === false || locked === undefined 
                 ? <FontAwesomeIcon icon={faLockOpenAlt} /> 
                 : <FontAwesomeIcon icon={faLockAlt} />}
+            </div>
+            <div className='button' onClick={() => { console.log("hey") }}>
+                <FontAwesomeIcon icon={faSearch} /> 
             </div>
         </div>
     )
