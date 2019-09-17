3b9938bc2e2bfd6f26620dc578010a7b965155b2
Luke Twomey <luke.twomey85@gmail.com>
2019-09-17 15:26:34 +0100

    Refactor shuffle helper function into separate file

--- client/src/reducers/mealPlanReducer.js
+++ client/src/reducers/mealPlanReducer.js
@@ -1,21 +1,13 @@
+import { shuffle } from '../helpers';
+
 const initialState = {recipes: []}
 
 export default (state = initialState, action) => {
     switch (action.type) {
         case 'PLAN_MEALS':
 
-            var shuffledRecipes = [...action.payload];
-            var m = shuffledRecipes.length, t, i;
-
-            // While there remain elements to shuffle…
-            while (m) {
-                // Pick a remaining element…
-                i = Math.floor(Math.random() * m--);
-                // And swap it with the current element.
-                t = shuffledRecipes[m];
-                shuffledRecipes[m] = shuffledRecipes[i];
-                shuffledRecipes[i] = t;
-            }
+            const allRecipes = [...action.payload];
+            const shuffledRecipes = shuffle(allRecipes);
 
             if({...state}.recipes.length === 0){
                 return { ...state, recipes: shuffledRecipes.slice(0,7) };
