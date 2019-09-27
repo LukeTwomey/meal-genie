18b9ecb6ab044a158e86d25cab4a63d68ee3e9a2
Luke Twomey <luke.twomey85@gmail.com>
2019-09-27 16:03:35 +0100

    Stop ios styling input elements

--- client/src/reducers/searchModalReducer.js
+++ client/src/reducers/searchModalReducer.js
@@ -1,7 +1,6 @@
 export default (state = { show: false, activeArrayIndex: null }, action) => {
     switch (action.type) {
         case 'TOGGLE_SEARCH_MODAL':
-            console.log("No, you got me");
             return {
                 ...state,
                 show: !state.show,
