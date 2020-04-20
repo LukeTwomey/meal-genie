8e37de404ff907442b1a9589148852cd727a2ab9
Luke Twomey <luke.twomey85@gmail.com>
2020-04-20 16:26:51 +0100

    Change email meal plan icon

--- client/src/components/MealPlan.js
+++ client/src/components/MealPlan.js
@@ -1,46 +1,40 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { toggleShareModal } from '../actions';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShare } from '@fortawesome/pro-solid-svg-icons';
+import { faEnvelopeOpenText } from '@fortawesome/pro-light-svg-icons';
+import { toggleShareModal } from '../actions';
 import RecipeCard from './recipes/RecipeCard';
 import Toolbar from './Toolbar';
 import './MealPlan.css';
 
 const Landing = (props) => {
-    
-    const recipes = props.mealPlan.recipes;
-    const toggleShareModal = props.toggleShareModal;
-    const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+  const { recipes } = props.mealPlan;
+  const { toggleShareModal } = props;
+  const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-    if (recipes === undefined || recipes.length === 0) {
-        return <p>Touch the genie to create a new meal plan!</p>;
-    } else {
-        return (
-            <div className="mealPlan">
-                {recipes.map((recipe, i) => {
-                    return (
-                        <div className='mealPlanCard' key={i}>
-                            <h1>{weekDays[i]}</h1>
-                            <Toolbar id={recipe._id} arrayIndex={i} locked={recipe.locked}/>
-                            <RecipeCard recipe={recipe} />
-                        </div>
-                    ) 
-                })}
-                <div className='shareButton' onClick={() => { toggleShareModal() }}>
-                    <FontAwesomeIcon icon={faShare} /> 
-                </div>
-            </div>
-        )
-    }
-}
+  if (recipes === undefined || recipes.length === 0) {
+    return <p>Touch the genie to create a new meal plan!</p>;
+  }
+  return (
+    <div className="mealPlan">
+      {recipes.map((recipe, i) => (
+        <div className="mealPlanCard" key={i}>
+          <h1>{weekDays[i]}</h1>
+          <Toolbar id={recipe._id} arrayIndex={i} locked={recipe.locked} />
+          <RecipeCard recipe={recipe} />
+        </div>
+      ))}
+      <div className="shareButton" onClick={() => { toggleShareModal(); }}>
+        <FontAwesomeIcon icon={faEnvelopeOpenText} />
+      </div>
+    </div>
+  );
+};
 
-const mapStateToProps = (state) => {
-    return { 
-        recipes: state.recipes,
-        loading: state.loading,
-        mealPlan: state.mealPlan
-    };
-}
+const mapStateToProps = (state) => ({
+  recipes: state.recipes,
+  loading: state.loading,
+  mealPlan: state.mealPlan,
+});
 
 export default connect(mapStateToProps, { toggleShareModal })(Landing);
