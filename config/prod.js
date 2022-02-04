b8a751d62f06033afc576e664ae4b46ba9dd5f01
Luke Twomey <luke.twomey@asos.com>
2022-02-04 15:56:46 +0000

    Add aws access details to prod key config file

--- config/prod.js
+++ config/prod.js
@@ -2,4 +2,8 @@ module.exports = {
   mongoURI: process.env.MONGODB_URI,
   gmailUser: process.env.GMAIL_USER,
   gmailPassword: process.env.GMAIL_PASSWORD,
+  awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  awsBucketName: process.env.AWS_BUCKET_NAME,
+  awsBucketRegion: process.env.AWS_BUCKET_REGION
 };
